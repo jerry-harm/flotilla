9ca73eb16260d8ba2cb9656da564b2f97d3a9d0f
Jon Staab <shtaab@gmail.com>
2026-07-03 16:50:12 -0700

    Add small badge

--- src/app/components/RoleBadge.svelte
+++ src/app/components/RoleBadge.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <div
-  class="badge w-fit min-w-0"
+  class="badge badge-sm w-fit min-w-0"
   style="background-color: {roleColorSoft(role.color)}; border-color: {roleColor(
     role.color,
   )}; color: {roleColor(role.color)};">
